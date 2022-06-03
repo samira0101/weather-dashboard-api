1c5071d5d92b8b2b547adb064359dceec7049626
Allure <samira-h@hotmail.co.uk>
2022-06-03 10:17:41 +0100

    location function container/radio button/label

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -33,3 +33,39 @@ var defineDisplayName = function(location) {
     return tempDisplayName.join(", ");
 }
 
+var confirmLocation = function(locationsArray) {
+    /* handle situtations where there are multiple results by surfacing a modal and prompting the user to choose a location */
+
+    // get the form body element and clear its contents
+    var formBody = confirmLocationModal.querySelector("#confirm-location-form-body");
+    formBody.innerHTML = "";
+
+    // set up the modal
+    for (let i=0; i < locationsArray.length; i++) {
+
+        // create the container
+        var searchResultContainer = document.createElement("div");
+        searchResultContainer.classList.add("search-result-item", "uk-form-controls", "uk-margin");
+
+        // create the radio button
+        var searchResultInput = document.createElement("input");
+        searchResultInput.setAttribute("type", "radio");
+        searchResultInput.setAttribute("name", "search-result");
+        searchResultInput.setAttribute("id", "search-result-" + i);
+        searchResultInput.setAttribute("data-location", JSON.stringify(locationsArray[i]));
+        searchResultContainer.appendChild(searchResultInput);
+
+        // create the label
+        var modalDisplayName = defineDisplayName(locationsArray[i]);
+        var searchResultLabel = document.createElement("label");
+        searchResultLabel.innerText = modalDisplayName;
+        searchResultLabel.setAttribute("for", "search-result-" + i);
+        searchResultContainer.appendChild(searchResultLabel);
+
+        // add the container to the form
+        formBody.appendChild(searchResultContainer);
+    }
+
+    // display the modal
+    UIkit.modal("#confirm-location-modal").show();
+}
