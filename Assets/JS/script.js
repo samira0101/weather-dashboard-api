c6031ad6c569b36c3ca2dcbc71b466b62fbb039d
Allure <samira-h@hotmail.co.uk>
2022-06-03 09:58:29 +0100

    load the dom elements

---
+++ Assets/JS/script.js
@@ -0,0 +1,9 @@
+
+// load the dom elements
+var searchInput = document.querySelector("#search-input");
+var searchButton = document.querySelector("#search-button");
+var confirmLocationModal = document.querySelector("#confirm-location-modal");
+var searchHistoryItems = document.querySelector("#search-history-items");
+var currentWeatherCity = document.querySelector("#current-weather-city");
+var currentWeatherData = document.querySelector("#current-weather");
+var forecastElement = document.querySelector("#forecast");
